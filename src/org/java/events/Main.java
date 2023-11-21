e66eaa52b97fda4fcbd8851225e4c398339ba59e
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-21 14:14:57 +0100

    added number available place on main class

--- src/org/java/events/Main.java
+++ src/org/java/events/Main.java
@@ -25,17 +25,62 @@ public class Main {
             System.out.print("Numero di posti totali: ");
             int postiTotali = in.nextInt();
 
-            // Creazione nuovo evento
+            // CREAZIONE NUOVO EVENTO
             Evento event = new Evento(titolo, data, postiTotali);
 
             System.out.println("Evento creato con successo:");
             System.out.println(event);
+            
+            //PRENOTAZIONI
 
-            event.prenota();
-            System.out.println("Prenotato un posto. Posti prenotati: " + event.getPostiPrenotati());
+            System.out.print("Desideri effettuare delle prenotazioni? (Si/No): ");
+            String answer = in.next();
 
-            event.disdici();
-            System.out.println("Disdetto un posto. Posti prenotati: " + event.getPostiPrenotati());
+            while (answer.equalsIgnoreCase("Si")) {
+
+            	System.out.print("Quanti posti vuoi prenotare? ");
+                int nPrenotazioni = in.nextInt();
+
+                // EFFETTURARE LA PRENOTAZIONE
+                for (int i = 0; i < nPrenotazioni; i++) {
+                    try {
+                        event.prenota();
+                        System.out.println("Prenotato un posto. Posti prenotati: " + event.getPostiPrenotati()
+                                + ", Posti disponibili: " + (event.getPostiTotali() - event.getPostiPrenotati()));
+                    } catch (IllegalStateException e) {
+                        System.err.println("Impossibile prenotare un posto: " + e.getMessage());
+                    }
+                }
+
+                System.out.print("Desideri effettuare altre prenotazioni? (Si/No): ");
+                answer = in.next();
+            }
+            
+            // DISDIRE PRENOTAZIONI
+            
+            System.out.print("Desideri disdire delle prenotazioni? (Si/No): ");
+            answer = in.next();
+
+            while (answer.equalsIgnoreCase("Si")) {
+
+            	System.out.print("Quanti posti vuoi disdire? ");
+                int nDisdette = in.nextInt();
+
+                // DISDIRE LA PRENOTAZIONE
+
+                for (int i = 0; i < nDisdette; i++) {
+                    try {
+                        event.disdici();
+                        System.out.println("Disdetto un posto. Posti prenotati: " + event.getPostiPrenotati()
+                                + " Posti disponibili: " + (event.getPostiTotali() - event.getPostiPrenotati()));
+                    } catch (IllegalStateException e) {
+                        System.err.println("Impossibile disdire un posto: " + e.getMessage());
+                    }
+                }
+
+                System.out.print("Desideri fare altre disdette? (Si/No): ");
+                answer = in.next();
+            }
 
         } catch (Exception e) {
             System.err.println("Errore: " + e.getMessage());
